dc37bbb30bd62b951835a85605fc6868863e7d30
ChristopheSoliiiid <marin.christophe@outlook.fr>
2017-02-28 17:11:40 +0100

    Ajout d'un script pour le Logo
    * Ajout du script pour réduire la taille de logo quand on scroll
    * Modification du text-shadow (15px -> 4px)

---
+++ Tests_bootstrap/js/reziseLogo.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+    if($(window).scrollTop() > 200) {
+        $('.logo').css({'width': '45px', 'transition-duration': '1.5s'});
+        $('.navbar-brand').css({'padding': '5px 15px', 'transition-duration': '1.5s'});
+    } else {
+        $('.logo').css({'width': '100px', 'transition-duration': '1.5s'});
+        $('.navbar-brand').css({'padding': '15px 15px', 'transition-duration': '1.5s'});
+    }
+});
