ed0d2c66ccbb66a131d65a8d6dc20a138a035763
ChristopheSoliiiid <marin.christophe@outlook.fr>
2017-03-01 09:47:47 +0100

    Modification du burger menu et script js
    * Modification du burger menu
    - couleur : darkorange
    - centrage du block en jouant avec les positions absolute/relative et
    top 50% transform -50%.
    * Désactivation du script resizeLogo pour les versions mobile.

--- Tests_bootstrap/js/reziseLogo.js
+++ Tests_bootstrap/js/reziseLogo.js
@@ -1,9 +1,13 @@
-$(window).scroll(function () {
-    if($(window).scrollTop() > 150) {
-        $('.logo').css({'width': '45px', 'transition-duration': '1.5s'});
-        $('.navbar-brand').css({'padding': '5px 15px', 'transition-duration': '1.5s'});
-    } else {
-        $('.logo').css({'width': '100px', 'transition-duration': '1.5s'});
-        $('.navbar-brand').css({'padding': '15px 15px', 'transition-duration': '1.5s'});
-    }
-});
+if (window.matchMedia("(min-width: 768px)").matches) {
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 150) {
+            $('.logo').css({'width': '45px', 'transition-duration': '1.5s'});
+            $('.navbar-brand').css({'padding': '5px 15px', 'transition-duration': '1.5s'});
+        } else {
+            $('.logo').css({'width': '100px', 'transition-duration': '1.5s'});
+            $('.navbar-brand').css({'padding': '15px 15px', 'transition-duration': '1.5s'});
+        }
+    });
+
+}
