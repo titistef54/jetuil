2b409e5241b687111ee08ea1f1a8f0d906fc74c7
ChristopheSoliiiid <marin.christophe@outlook.fr>
2017-03-23 11:03:58 +0100

    Skipper et js

--- Tests_bootstrap/js/reziseLogo.js
+++ Tests_bootstrap/js/reziseLogo.js
@@ -2,11 +2,11 @@ if (window.matchMedia("(min-width: 1101px)").matches) {
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 150) {
-            $('.logo').css({'width': '45px', 'transition-duration': '1.5s'});
-            $('.navbar-brand').css({'padding': '5px 15px', 'transition-duration': '1.5s'});
+            $('.logo').css({'width': '45px', 'transition-duration': '0.5s'});
+            $('.navbar-brand').css({'padding': '5px 15px', 'transition-duration': '0.5s'});
         } else {
-            $('.logo').css({'width': '100px', 'transition-duration': '1.5s'});
-            $('.navbar-brand').css({'padding': '15px 15px', 'transition-duration': '1.5s'});
+            $('.logo').css({'width': '100px', 'transition-duration': '0.5s'});
+            $('.navbar-brand').css({'padding': '15px 15px', 'transition-duration': '0.5s'});
         }
     });
 
